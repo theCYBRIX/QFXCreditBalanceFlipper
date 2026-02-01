97c0928b3e7d24175dc16fe845de62677f9cdc8a
theCYBRIX <102550532+theCYBRIX@users.noreply.github.com>
2026-02-01 00:11:05 -0600

    refactor: split main function logic into smaller functions chore: added module and method docstrings feat: added undo flag feat: created simplistic logo for the project

--- credit_balance_flipper.py
+++ credit_balance_flipper.py
@@ -1,3 +1,22 @@
+"""
+This module flips the credit balance found in QFX files to be negative
+to reflect a debt in financial apps such as Quicken.
+
+Args:
+    file_path... (str): The path/s to the .qfx file/s to process.
+
+Flags:
+    -p, --pause     Wait for user input before exiting
+    -u, --undo      Make credit balance positive in the specified files
+    -h, --help      Show help message
+
+This module can be called via the command line, or by dragging and dropping one
+or multiple files onto the executable.
+
+Example command line usage:
+    .\\credit_balance_flipper.exe ".\\transactions.qfx" ".\\transactions(1).qfx" --pause
+"""
+
 import os
 import argparse
 from colorama import Fore, Style
@@ -8,15 +27,23 @@ END_CATEGORY: str = "</LEDGERBAL>"
 PROPERTY_NAME: str = "<BALAMT>"
 
 
+def main():
+    """
+    Initializes the main logic of the program then waits for user input if required
+    """
+    args = parse_args()
+    success = process_files(args.files, args.undo)
+    if args.pause or not success:
+        input("Press Enter to continue...")
+
+
 def parse_args():
+    """
+    Uses argparse to parse command line arguments
+    """
+
     parser = argparse.ArgumentParser(
         description="Convert credit card balance in .qfx files to a negative value."
-    )
-
-    parser.add_argument(
-        "-p", "--pause",
-        action="store_true",
-        help="Wait for user input before exiting"
     )
 
     parser.add_argument(
@@ -26,86 +53,141 @@ def parse_args():
         help="File/s to process (drag and drop supported)"
     )
 
+    parser.add_argument(
+        "-p", "--pause",
+        action="store_true",
+        help="Wait for user input before exiting"
+    )
+
+    parser.add_argument(
+        "-u", "--undo",
+        action="store_false",
+        help="Make credit balance positive in the specified files"
+    )
+
     return parser.parse_args()
 
 
-def main():
-    args = parse_args()
-    cmd_line_paths: list[str] = args.files
-    pause: bool = args.pause
+def process_files(
+        file_paths : list[str], make_balance_negative : bool = True):
+    """
+    Validates file paths and types and updates the credit balance signage
+    in files as specified by make_balance_negative.
+    
+    Parameters:
+        file_paths (list[str]): List of files in which to flip credit balance value
+        make_balance_negative: Weather to make credit balance negative (default) or positive
+    
+    Returns:
+        success (bool): Weather all files could be updated/validated successfully
+    """
 
-    if len(cmd_line_paths) == 0:
+    if not file_paths:
         raise ValueError(
-            "No file specified.\nUsage: CreditBalanceFlipper \"filepath\"...")
+            "No file specified.\nUsage: CreditBalanceFlipper \"filepath\"..."
+        )
 
-    for path in cmd_line_paths:
+    for path in file_paths:
+        if not os.path.exists(path):
+            raise ValueError(
+                "Specified file does not exist: "
+                + os.path.basename(path)
+            )
         if not path.endswith(".qfx"):
             raise ValueError(
-                "Specified file does not appear to be in qfx format: " + os.path.basename(path))
+                "Specified file does not appear to be in qfx format: "
+                + os.path.basename(path)
+            )
 
     success_count: int = 0
 
-    for path in cmd_line_paths:
-        file_path: str = path
+    for path in file_paths:
         file_contents: list[str]
 
-        with open(file_path, "r", encoding="UTF-8") as file:
+        with open(path, "r", encoding="UTF-8") as file:
             file_contents = file.readlines()
 
-        inside_category: bool = False
-        value_found: bool = False
-        value_flipped: bool = False
+        value_found, value_updated = correct_balance(file_contents, make_balance_negative)
 
-        for i in range(len(file_contents)):
-            line: str = file_contents[i]
-
-            if START_CATEGORY in line:
-                inside_category = True
-
-            elif PROPERTY_NAME in line and inside_category:
-                value_found = True
-
-                value_index: int = line.find(PROPERTY_NAME) + len(PROPERTY_NAME)
-                value = line[value_index:].strip()
-                as_float = float(value)
-
-                if as_float > 0:
-                    value_flipped = True
-
-                line = line.replace(value, str(-abs(as_float)))
-                file_contents[i] = line
-                break
-
-            elif END_CATEGORY in line:
-                inside_category = False
+        if value_updated:
+            with open(path, "w", encoding="UTF-8") as file:
+                file.writelines(file_contents)
 
         if not value_found:
-            print(Fore.YELLOW + "[Warning]" + Style.RESET_ALL +
-                  " Unable to locate value to flip in: \"{}\"".format(os.path.basename(file_path)))
+            print(
+                f"{Fore.YELLOW}[Warning]{Style.RESET_ALL}" +
+                f" Unable to locate value to flip in: \"{os.path.basename(path)}\""
+            )
             continue
 
         success_count += 1
 
-        if value_flipped:
-            with open(file_path, "w", encoding="UTF-8") as file:
-                file.writelines(file_contents)
+        print(f"{Fore.GREEN}[OK]{Style.RESET_ALL} " +
+            (
+                "Flipped value"
+                if value_updated else
+                f"Balance already {"negative" if make_balance_negative else "positive"}"
+            )
+            + f" in: \"{os.path.basename(path)}\""
+        )
 
-        print(Fore.GREEN + "[OK] " + Style.RESET_ALL +
-              ("Flipped value" if value_flipped else "Balance already negative") +
-              " in: \"{}\"".format(os.path.basename(file_path)))
-
-    if success_count == len(cmd_line_paths):
+    if success_count == len(file_paths):
         print(Fore.GREEN + "Process completed Successfully." + Style.RESET_ALL)
-    else:
-        print(Fore.GREEN + "Finished." + Style.RESET_ALL +
-              (" No files could be updated. [{}/{}]" if success_count == 0 else
-               " Updated/Validated {} of {} files successfully.")
-              .format(success_count, len(cmd_line_paths))
-              )
-        pause = True
+        return True
 
-    if pause:
-        input("Press Enter to continue...")
+    print(Fore.GREEN + "Finished. " + Style.RESET_ALL +
+            (
+                f"No files could be updated. [{success_count}/{len(file_paths)}]"
+                if success_count == 0 else
+                f"Updated/Validated {success_count} of {len(file_paths)} files successfully."
+            )
+        )
+
+    return False
+
+
+def correct_balance(file_contents : list[str], make_negative : bool = True):
+    """
+    Finds and corrects the credit balance value if needed. 
+    
+    Parameters:
+        file_contents (list[str]): File contents in which to correct the credit balance signage
+        make_negative (bool): Weather to make the credit balance negative (default) or positive
+
+    returns:
+        value_found (bool): Weather the credit balance could be found
+        value_corrected (bool): Weather the credit balance needed to be corrected
+    """
+
+    inside_category: bool = False
+    value_found: bool = False
+    value_flipped: bool = False
+
+    for index, line in enumerate(file_contents):
+
+        if START_CATEGORY in line:
+            inside_category = True
+
+        elif PROPERTY_NAME in line and inside_category:
+            value_found = True
+
+            value_index: int = line.find(PROPERTY_NAME) + len(PROPERTY_NAME)
+            value = line[value_index:].strip()
+            as_float = float(value)
+
+            if (as_float > 0 and make_negative) or (as_float < 0 and not make_negative):
+                value_flipped = True
+                line = line.replace(value, str(
+                    (-abs(as_float)) if make_negative else abs(as_float)
+                ))
+                file_contents[index] = line
+
+            break
+
+        elif END_CATEGORY in line:
+            inside_category = False
+
+    return value_found, value_flipped
 
 
 if __name__ == "__main__":
